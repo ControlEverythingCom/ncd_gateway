283593c0a9f07f433e0fea8a79698c2ea690ca60
zaphod-42 <trey@tomadoh.com>
2016-04-28 13:03:57 -0500

    Create i2c_comm.h

---
+++ firmware/i2c_comm.h
@@ -0,0 +1,9 @@
+#include "spark_wiring.h"
+#include "spark_wiring_constants.h"
+#include "spark_wiring_usbserial.h"
+#include "spark_wiring_i2c.h"
+
+int processCommandI2C(int addr, String command);
+int writeCommandI2C(int addr, String command);
+int writeCommandsI2C(int addr, String command);
+int readCommandI2C(int addr, String command, byte *buf, int size);
