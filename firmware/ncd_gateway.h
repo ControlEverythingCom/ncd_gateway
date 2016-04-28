f352e2f2fcdd02f7b6022e44c853cedd5342ff31
zaphod-42 <trey@tomadoh.com>
2016-04-28 13:03:20 -0500

    Update ncd_gateway.h

--- firmware/ncd_gateway.h
+++ firmware/ncd_gateway.h
@@ -1,1 +1,8 @@
 
+#include "spark_wiring.h"
+#include "spark_wiring_constants.h"
+#include "spark_wiring_usbserial.h"
+#include "i2c_comm.h"
+
+int bytesToInt(byte* b, int size);
+int gatewayCommand(String command);
