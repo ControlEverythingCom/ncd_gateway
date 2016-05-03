24e77e4e90f1b9262aaeec837e911b36f7c4fecb
zaphod-42 <trey@tomadoh.com>
2016-05-03 11:20:07 -0500

    Update ncd_gateway.cpp

--- firmware/ncd_gateway.cpp
+++ firmware/ncd_gateway.cpp
@@ -2,6 +2,24 @@
 #include "S3B.h"
 
 S3B sModule;
+String eventReturns[5];
+
+int sendEvent(String key){
+    int i = key.toInt();
+    String eventName = "";
+    eventName = "eventReturn_"+key;
+    Particle.publish(eventName, eventReturns[i], 60, PRIVATE);
+    eventReturns[i] = "";
+    return 1;
+};
+int setEventReturn(String value){
+    int index = 0;
+    while(eventReturns[index].length() > 1){
+        index++;
+    }
+    eventReturns[index] = value;
+    return index;
+};
 
 //i2c,32,writem,0,252|6,252
 int gatewayCommand(String command){
@@ -23,27 +41,33 @@ int gatewayCommand(String command){
     Serial.println(address);
     
     if(type.equals("i2c")){
-        int addr = address.toInt();
         if(func.equals("read")){
             s = args.indexOf(',');
             int size = args.substring(0, s).toInt();
             args = args.substring(s+1);
             byte buff[size];
+            int addr = address.toInt();
             readCommandI2C(addr, args, buff, size);
-            
-            Serial.print("Size: ");
             Serial.println(size);
             //maybe check if it's more than 4 bytes here and set an event since the client should already know to listen
-            response = bytesToInt(buff, size);
+            
+            if(size>3){
+                byte rebytes[4] = {0xff,0xff,0xff,0x00};
+                String byteString = "";
+                int i;
+                for(i=0;i<size;i++){
+                    if(!byteString.equals("")){
+                        byteString = byteString + ",";
+                    }
+                    byteString = byteString + String(buff[i]);
+                }
+                rebytes[3] = setEventReturn(byteString);
+                response = bytesToInt(rebytes, 4);
+            }else{
+                response = bytesToInt(buff, size);
+            }
         }
         else if(func.equals("write")){
-            
-            Serial.print(addr);
-	        Serial.println(".");
-	        
-            Serial.print(args);
-	        Serial.println(".");
-	        
             response = writeCommandsI2C(addr, args);
         }
     }
@@ -87,4 +111,15 @@ int bytesToInt(byte* b, int size){
       val = val | b[i] << ((i)*8);
     }
     return val;
+}
+
+void dpm(String name, String arg){
+    Serial.print(name);
+    Serial.print(": ");
+    Serial.println(arg);
+}
+void dpm(String name, int arg){
+    Serial.print(name);
+    Serial.print(": ");
+    Serial.println(arg);
 }
