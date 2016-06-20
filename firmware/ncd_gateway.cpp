16acf673750b8a5ad54b33648067e45514689d99
zaphod-42 <trey@tomadoh.com>
2016-06-20 14:56:35 -0500

    Update ncd_gateway.cpp

--- firmware/ncd_gateway.cpp
+++ firmware/ncd_gateway.cpp
@@ -1,17 +1,173 @@
 #include "ncd_gateway.h"
 #include "S3B.h"
 #include "spark_wiring_eeprom.h"
-#include "SparkJson/SparkJson.h"
 
 S3B sModule;
 String eventReturns[5];
+unsigned long tOut = 3000;
 
-int initCommands(JsonObject& root);
+void commandHandler(const char *event, const char *data);
 
-int i2c(JsonObject& root);
-int s3b(JsonObject& root);
+int gatewayCommand(String command);
 
+int ncdApi(byte packetBytes[]);
+void i2c_command(byte bytes[], byte *buff);
 
+void array_slice(byte bytes[], int start, int len, byte *buff);
+void array_slice(byte bytes[], int start, int len, int *buff);
+
+void init_gateway();
+
+//int s3b(JsonObject& root);
+
+void init_gateway(){
+    Particle.function("deviceComm", gatewayCommand);
+    Particle.subscribe("ncd_deviceCom", commandHandler, MY_DEVICES);
+    Serial1.begin(115200);
+    Wire.begin();
+}
+
+int gatewayCommand(String arg){
+    int length = arg.length();
+    byte bytes[length+1];
+    arg.getBytes(bytes, length+1);
+    //send int pointer for setting to allow me to test how to return results?
+    return ncdApi(bytes);
+}
+
+void commandHandler(const char *event, const char *data){
+    Serial.println("Got Event "+String(event));
+    String newCommand = String(data);
+    gatewayCommand(newCommand);
+}
+int ncdApi(byte packetBytes[]){
+    int buffLen = 1;
+    Serial.println(String(packetBytes[0]));
+    switch(packetBytes[0]){
+        case 187:
+            {
+                //packet of packets
+                int i=2;
+                int max;
+                for(int pn = 0; pn<packetBytes[1]; pn++){
+                    
+                    max=i+packetBytes[i];
+                    Serial.println(max);
+                    byte intPacket[packetBytes[i]];
+                    i++;
+                    int ni=0;
+                    for(i;i<=max;i++){
+                        intPacket[ni]=packetBytes[i];
+                        ni++;
+                    }
+                    ncdApi(intPacket);
+                }
+                break;
+            }
+        case 188:
+            {
+                //plain i2c w/r command
+                if(packetBytes[3] > 0){
+                    buffLen = packetBytes[3];
+                }
+                byte buff[buffLen];
+                i2c_command(packetBytes, buff);
+                break;
+            }
+        case 189:
+            {
+                //masking command
+                int addr = packetBytes[1];
+                int maskOp = packetBytes[2];
+                int maskedOffsets = packetBytes[3];
+                int readCommandLen = packetBytes[4];
+                int readLen = packetBytes[5];
+                int readCommand[readCommandLen];
+                
+                array_slice(packetBytes, 6, readCommandLen, readCommand);
+                
+                int writeCommandLen = packetBytes[6+readCommandLen];
+                int writeCommand[writeCommandLen];
+                
+                array_slice(packetBytes, 7+readCommandLen, writeCommandLen, writeCommand);
+                
+                
+                int writeVals[writeCommandLen];
+                int wi=0;
+                for(wi; wi<maskedOffsets; wi++){
+                    writeVals[wi]=writeCommand[wi];
+                    Serial.println(writeVals[wi]);
+                }
+                
+                writeCommandsI2C(addr, readCommand, readCommandLen);
+                Wire.requestFrom(addr, readLen);
+                for(int i=0;i<readLen;i++){
+                    int current = Wire.read();
+                    writeVals[wi] = mask(current, writeCommand[wi], maskOp);
+                    Serial.println(current);
+                    Serial.println(writeVals[wi]);
+                    wi++;
+                }
+                
+                writeCommandsI2C(addr, writeVals, writeCommandLen);
+                break;
+            }
+    }
+    return 1;
+}
+int mask(int val, int mask, int type){
+    switch(type){
+        case 0:
+            val |= mask;
+            break;
+        case 1:
+            val &= mask;
+            break;
+        case 2:
+            val ^= mask;
+            break;
+        case 3:
+            val = val << mask;
+            break;
+        case 4:
+            val = val >> mask;
+    }
+    return val;
+}
+void i2c_command(byte bytes[], byte *buff){
+    if(bytes[0] == 188){
+        int commands[bytes[2]];
+        array_slice(bytes, 4, bytes[2], commands);
+        int addr = bytes[1];
+        if(bytes[3] == 0){
+            //write command
+            buff[0]=writeCommandsI2C(addr, commands, bytes[2]);
+        }else{
+            //read command
+            writeCommandsI2C(addr, commands, bytes[2]);
+            Wire.requestFrom(addr, bytes[3]);
+            for(int i=0;i<bytes[3];i++){
+                buff[i] = Wire.read();
+            }
+        }
+    }
+}
+void array_slice(byte bytes[], int start, int len, byte *buff){
+    int ni = 0;
+    int end = start+len;
+    for(int i=start; i<=end; i++){
+        buff[ni] = bytes[i];
+        ni++;
+    }
+}
+void array_slice(byte bytes[], int start, int len, int *buff){
+    int ni=0;
+    int end = start+len;
+    for(int i=start; i<=end; i++){
+        buff[ni] = bytes[i];
+        ni++;
+    }
+}
 int sendEvent(String key){
     int i = key.toInt();
     String eventName = "";
@@ -28,152 +184,3 @@ int setEventReturn(String value){
     eventReturns[index] = value;
     return index;
 };
-
-//i2c,32,writem,0,252|6,252
-
-//S3B command format
-//rs,0.19.162.0.65.2.32.216,wi,0,252|6,252
-
-int gatewayCommand(String command){
-    
-    DynamicJsonBuffer jsonBuffer;
-        
-    Serial.println("Received: "+command);
-    
-    char commandData[command.length()+1];
-    command.toCharArray(commandData, command.length()+1);
-    
-    //Parse incomming JSON
-    JsonObject& root = jsonBuffer.parseObject(commandData);
-    
-    //Check to see if parsing Json object was successful.
-    if(!root.success()){
-        Serial.println("Failed to parse JSON");
-        return 0;
-    }
-    for(int i = 0; i < root["root"].size(); i++){
-        //Check type
-        const char* typeChar = root["root"][i]["type"];
-        String type = String(typeChar);
-        Serial.println("type: "+type);
-        
-        if(type.equals("init")){
-            initCommands(root["root"][i]);
-        }
-        if(type.equals("i2c")){
-            Serial.println("I2C Command");
-            i2c(root["root"][i]);
-        }
-        if(type.equals("s3b")){
-            s3b(root["root"][i]);
-        }
-    }
-    Serial.println("Done");
-    return 0;
-}
-
-int bytesToInt(byte* b, int size){
-    int val = 0;
-    for(int i=3;i>-1;i--){ 
-      val = val | b[i] << ((i)*8);
-    }
-    return val;
-}
-
-void dpm(String name, String arg){
-    Serial.print(name);
-    Serial.print(": ");
-    Serial.println(arg);
-}
-void dpm(String name, int arg){
-    Serial.print(name);
-    Serial.print(": ");
-    Serial.println(arg);
-}
-
-int i2c(JsonObject& root){
-    int commandsRan = 0;
-    for(int i = 0; i < root["args"].size(); i++){
-        int addr = root["args"][i]["address"];
-        Serial.printf("i2c sending command to address: %i data: ", addr);
-        for(int j = 0; j < root["args"][i]["commands"].size(); j++){
-            int data[root["args"][i]["commands"][j].size()];
-            for(int k = 0; k < root["args"][i]["commands"][j].size(); k++){
-                data[k] = root["args"][i]["commands"][j][k];
-                Serial.printf("%i, ", data[k]);
-            }
-            Serial.println();
-            writeCommandsI2C(addr, data, root["args"][i]["commands"][j].size());
-            commandsRan++;
-        }
-    }
-    Serial.printf("i2c Ran %i commands \n",commandsRan);
-    return commandsRan;
-}
-
-int s3b(JsonObject& root){
-    
-}
-
-//Returns the number of init commands stored to memory
-//Format init,i2c,addr,write,arg1,arg2|i2c,addr,write,arg1,arg2
-int initCommands(JsonObject& root){
-    Serial.println("Init Command");
-    
-    char initCommandsToStore[255];
-    root.printTo(initCommandsToStore, 255);
-    String initCommandString = String(initCommandsToStore);
-    Serial.printf("Length of initCommandString: %i \n", initCommandString.length());
-
-    Serial.print("initCommand: ");
-    Serial.println(initCommandsToStore);
-    
-    int sInd = 0;
-    EEPROM.write(sInd, initCommandString.length());
-    sInd++;
-    for(int i = 0; i < initCommandString.length(); i++){
-        EEPROM.write(sInd, initCommandsToStore[i]);
-        sInd++;
-    }
-    Serial.printf("stored %i chars \n", sInd);
-}
-
-//Returns the number of init commands ran.
-int runInitCommands(){
-    if(EEPROM.read(0) == 255){
-        Serial.println("No Init Commands Stored");
-        return 0;
-    }
-    int numberOfCommandsRan = 0;
-    
-    int ind = 0;
-    int commandStringLen = EEPROM.read(ind);
-    ind++;
-    char commandString[commandStringLen+1];
-    for(int i = 0; i < commandStringLen; i++){
-        commandString[i] = EEPROM.read(ind);
-        ind++;
-    }
-    Serial.print("Stored Init: ");
-    Serial.println(commandString);
-    DynamicJsonBuffer jsonBuffer;
-    JsonObject& root = jsonBuffer.parseObject(commandString);
-    
-    //Check to see if parsing Json object was successful.
-    if(!root.success()){
-        Serial.println("Failed to parse init JSON");
-        return 0;
-    }else{
-        Serial.println("Parse init JSON success");
-    }
-    i2c(root);
-    return numberOfCommandsRan;
-}
-
-int clearInit(String args){
-    for(int i = 0; i < 2047; i++){
-        EEPROM.write(i, 255);
-    }
-    return 1;
-}
-
