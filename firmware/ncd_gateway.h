48f1573b786722cb980acbe580f9b4787602998a
zaphod-42 <trey@tomadoh.com>
2016-09-29 13:29:32 -0500

    Update ncd_gateway.h

--- firmware/ncd_gateway.h
+++ firmware/ncd_gateway.h
@@ -1,6 +1,7 @@
 #include "spark_wiring.h"
 #include "spark_wiring_constants.h"
 #include "spark_wiring_usbserial.h"
+#include "spark_wiring_usartserial.h"
 #include "spark_wiring_i2c.h"
 
 void commandHandler(const char *event, const char *data);
@@ -8,14 +9,15 @@ void commandHandler(const char *event, const char *data);
 int gatewayCommand(String command);
 
 int ncdApi(byte packetBytes[]);
+int ncdApi(byte packetBytes[], bool ka);
 void i2c_command(byte bytes[], byte *buff);
 int writeCommandsI2C(int addr, int* commands, int commandsLen);
-
 void array_slice(byte bytes[], int start, int len, byte *buff);
 void array_slice(byte bytes[], int start, int len, int *buff);
 int mask(int val, int mask, int type);
 
 void init_gateway();
+void loop_gateway();
 int hexToInt(String arg, byte bytes[], int length);
 int base64ToInt(String arg, byte buff[], int length);
 int bytesToInt(byte bytes[], int length);
